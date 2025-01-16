5f94dc75321a20afba92b3b8ac7e325e43fb0344
Sang Hun Park <62207757+bp4sp4@users.noreply.github.com>
2025-01-16 17:00:12 +0900

    Updates

--- components/imges.tsx
+++ components/imges.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from "react";
 import imagePaths from "../../jsondata/imagePaths.json";
-import styles from "../css/image.module.css";
+import styles from "../../app/styles/image.module.css";
 
 export default function Images() {
   useEffect(() => {
