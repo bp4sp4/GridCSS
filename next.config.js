19a0adc5a0a17c55c352537dd7e6b0629c59080f
Sang Hun Park <62207757+bp4sp4@users.noreply.github.com>
2025-01-16 15:02:24 +0900

    배포 settings

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-module.exports = {
-  basePath: "/GridCSS",
-  assetPrefix: "/GridCSS",
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export", // 정적 파일 생성 모드 활성화
+  basePath: "/GridCSS", // 리포지토리 이름
+  assetPrefix: "/GridCSS/", // 정적 자산 경로 설정
 };
+
+module.exports = nextConfig;
